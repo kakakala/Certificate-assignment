164e7bd4db5a72c653b1e4ec91fba308d62f70a1
kakakala <53092552+kakakala@users.noreply.github.com>
2023-06-03 13:38:06 +0800

    Add files via upload

---
+++ 切版任務作業二 - AI 工具王/js/切版任務作業二 - AI 工具王.js
@@ -0,0 +1,8 @@
+$(document).ready(function () {
+    $('#scrollToTop').click(function () { 
+        $('html, body').animate({
+            scrollTop: 0
+        }, 800);
+        
+    });
+    });
