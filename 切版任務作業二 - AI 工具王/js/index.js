a815378b7d45cc34ddad4b4b89c1631b01af51e2
kakakala <53092552+kakakala@users.noreply.github.com>
2023-06-11 11:44:05 +0800

    Delete 切版任務作業二 - AI 工具王 directory

--- 切版任務作業二 - AI 工具王/js/index.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function () {
-    $('#scrollToTop').click(function () { 
-        e.preventDefault();
-        $('html, body').animate({
-            scrollTop: 0
-        }, 800);
-        
-    });
-    });
